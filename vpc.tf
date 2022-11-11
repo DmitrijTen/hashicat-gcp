ec00b4dcf9b0bf1b4a97f0fd7267e9fae63db8ae
DmitrijTen <dmitrij.ten@devoteam.com>
2022-11-11 13:53:49 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,12 +3,12 @@ module "vpc" {
     version = "~> 3.0"
 
     project_id   = var.project_id
-    network_name = "gurav-network"
+    network_name = "gaurav-network"
     routing_mode = "GLOBAL"
 
     subnets = [
         {
-            subnet_name           = "gurav-subnet"
+            subnet_name           = "gaurav-subnet"
             subnet_ip             = "10.100.10.0/24"
             subnet_region         = var.region
         }
