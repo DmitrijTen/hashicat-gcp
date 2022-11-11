79e4d24e005f9bade303e1a279b81bb31f6a6914
Dmitrij Ten <97442269+DmitrijTen@users.noreply.github.com>
2022-11-11 13:48:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,9 +69,9 @@ resource "google_compute_instance" "hashicat" {
   tags = ["http-server"]
 
   labels = {
-    name = "hashicat",
-    department   = "devops",
-    billable     = true
+    name = "hashicat"
+    "department" = "devops"
+    "billable" = true
   }
 
 }
